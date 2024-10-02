d59e820e43f8c819e6b1e31004d51b331d8140ff
BoopathiC <cboopathipnr@gmail.com>
2024-10-02 18:35:39 +0530

    updated

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter here
 import Navbar from './Navbar';
 import HeroSection from './HeroSection';
 // Import other components here
